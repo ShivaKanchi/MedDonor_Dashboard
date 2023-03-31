1b08233562c07b663e2c00aa00369cbfc971e4d5
ShivaKanchi <shivakanchi111@gmail.com>
2023-03-31 22:31:09 +0530

    Working with redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import react from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from "./Redux/store"
 import './index.css'
+
 import App from './App';
 import { ContextProvider } from './contexts/ContextProvider';
+
 ReactDOM.render(
-    <ContextProvider>
-        <App />
-    </ContextProvider>
+    <BrowserRouter>
+        <Provider store={store}>
+            <ContextProvider>
+                <App />
+            </ContextProvider>
+        </Provider>
+    </BrowserRouter>
     , document.getElementById('root')
 )
--- src/pages/Medicines.jsx
+++ src/pages/Medicines.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids'
+import { customersData, customersGrid } from '../data/dummy'
+import { Header } from '../components'
+
+
+import { useDispatch, useSelector } from "react-redux";
+import { getMedicines } from "../redux/Reducers/Medicine/medicine.action";
+
 
 const Medicines = () => {
+    const [medicines, setMedicines] = useState([])
+
+    const dispatch = useDispatch()
+    // const medData = useSelector((state) => state.medicine.medicinelist)
+
+    useEffect(() => {
+        dispatch(getMedicines())
+    }, [])
+
+    // useEffect(() => {
+    //     if (medData) {
+    //         setMedicines(medData);
+    //     }
+    // }, [medData]);
+
     return (
-        <div>Medicines</div>
+        <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+            <Header category="Page" title="Customers" />
+            <GridComponent
+                dataSource={customersData}
+                allowPaging
+                allowSorting
+                toolbar={['Delete']}
+                editSettings={{ allowDeleting: true, allowEditing: true }}
+                width="auto"
+            >
+                <ColumnsDirective>
+                    {customersGrid.map((item, index) => (
+                        <ColumnDirective key={index}{...item} />
+                    ))}
+                </ColumnsDirective>
+                <Inject services={[Page, Toolbar, Selection, Edit, Sort, Filter]} />
+            </GridComponent>
+        </div >
     )
 }
 
