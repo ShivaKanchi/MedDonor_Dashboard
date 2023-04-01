240123e0b94d95f2585f6d4c63a21341874a18ae
ShivaKanchi <shivakanchi111@gmail.com>
2023-04-01 18:53:31 +0530

    Added medicines records to dashboard

--- src/pages/Medicines.jsx
+++ src/pages/Medicines.jsx
@@ -9,12 +9,13 @@ const customerGridImage = (props) => (
     <div className="image flex gap-4">
         <img
             className="rounded-full w-10 h-10"
-            src={props.CustomerImage}
-            alt="employee"
+            src={props.medimage}
+            alt="med"
         />
         <div>
-            <p>{props.CustomerName}</p>
-            <p>{props.CustomerEmail}</p>
+            <p>Donor</p>
+            <p>{props.donorname}</p>
+            {/* <p>{props.CustomerEmail}</p> */}
         </div>
     </div>
 );
@@ -24,50 +25,125 @@ const customerGridStatus = (props) => (
         <p>{props.Status}</p>
     </div>
 );
-export const customersData = [
+export const medicineData = [
     {
-        CustomerID: 1001,
-        CustomerName: 'Nirav Joshi',
-        CustomerEmail: 'nirav@gmail.com',
-
-        ProjectName: 'Hosting Press HTML',
-        Status: 'Active',
-        StatusBg: '#8BE78B',
-        Weeks: '40',
-        Budget: '$2.4k',
-        Location: 'India',
-    },
-    {
-        CustomerID: 1002,
-
-        CustomerName: 'Sunil Joshi',
-        CustomerEmail: 'sunil@gmail.com',
-        ProjectName: 'Elite Admin',
-        Status: 'Active',
-
-        StatusBg: '#8BE78B',
-        Weeks: '11',
-        Budget: '$3.9k',
-        Location: 'India',
-    },
-    {
-        CustomerID: 1003,
-
-        CustomerName: 'Andrew McDownland',
-        CustomerEmail: 'andrew@gmail.com',
-        ProjectName: 'Real Homes WP Theme',
-        Status: 'Pending',
-
-        StatusBg: '#FEC90F',
-        Weeks: '19',
-        Budget: '$24.5k',
-        Location: 'USA',
-    },
-   
-
+        _id: "63fb96806cdf6224160d6cae",
+        medname: "Saridon Headache Relief Tablet ",
+        medimage: "https://www.netmeds.com/images/product-v1/600x600/109486/saridon_headache_relief_tablet_10s_117986_0_3.jpg",
+        category: "Headache",
+        desc: "Only to take decrese headache",
+        donor: "63fb6fcea91936bad7c05e9a",
+        donorname: "SHIVA Kanchi",
+        donorimage: "https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/12/femto-berserk.jpg",
+        expiry: "2023-1-10",
+        quantity: "2",
+        address: "UCOE",
+        phone: "8928656498",
+        comments: [],
+    }
 ];
 
 export const medicineGrid = [
+    { type: 'checkbox', width: '50' },
+    {
+        field: '_id',
+        headerText: 'Medicine ID',
+        width: '120',
+        textAlign: 'Center',
+        isPrimaryKey: true,
+    },
+    {
+        field: 'medname',
+        headerText: 'Name',
+        width: '120',
+        textAlign: 'Center',
+    },
+    {
+        headerText: 'Donor',
+        width: '150',
+        template: customerGridImage,
+        textAlign: 'Center'
+    },
+    {
+        field: 'desc',
+        headerText: 'Description',
+        width: '150',
+        textAlign: 'Center'
+    },
+    // {
+    //     field: 'expiry',
+    //     headerText: 'Expiry',
+    //     width: '130',
+    //     format: 'yMd',
+    //     textAlign: 'Center',
+    //     template: customerGridStatus
+    // },
+    {
+        field: 'quantity',
+        headerText: 'Quantity',
+        width: '100',
+        format: 'C2',
+        textAlign: 'Center'
+    },
+    {
+        field: 'phone',
+        headerText: 'Phone',
+        width: '100',
+        format: 'yMd',
+        textAlign: 'Center'
+    },
+
+    {
+        field: 'address',
+        headerText: 'Location',
+        width: '150',
+        textAlign: 'Center'
+    },
+
+
+
+];
+
+const Medicines = () => {
+    const [medicines, setMedicines] = useState([])
+    const dispatch = useDispatch()
+    const medData = useSelector((state) => state.medicine.medicinelist)
+    useEffect(() => {
+        dispatch(getMedicines())
+    }, [])
+    useEffect(() => {
+        if (medData) {
+            setMedicines(medData);
+        }
+    }, [medData]);
+
+    return (
+        <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+            <Header category="Page" title="Medicines" />
+            <GridComponent
+                dataSource={medicines}
+                allowPaging
+                allowSorting
+                toolbar={['Delete']}
+                editSettings={{ allowDeleting: true, allowEditing: true }}
+                width="auto"
+            >
+                <ColumnsDirective>
+                    {medicineGrid.map((item, index) => (
+                        <ColumnDirective key={index}{...item} />
+                    ))}
+                </ColumnsDirective>
+                <Inject services={[Page, Toolbar, Selection, Edit, Sort, Filter]} />
+            </GridComponent>
+        </div >
+    )
+}
+
+export default Medicines
+
+
+
+export const customerGrid = [
     { type: 'checkbox', width: '50' },
     {
         headerText: 'Name',
@@ -120,42 +196,43 @@ export const medicineGrid = [
     },
 
 ];
+export const customersData = [
+    {
+        CustomerID: 1001,
+        CustomerName: 'Nirav Joshi',
+        CustomerEmail: 'nirav@gmail.com',
 
-const Medicines = () => {
-    const [medicines, setMedicines] = useState([])
-    const dispatch = useDispatch()
-    const medData = useSelector((state) => state.medicine.medicinelist)
-    useEffect(() => {
-        dispatch(getMedicines())
-    }, [])
-    useEffect(() => {
-        if (medData) {
-            setMedicines(medData);
-        }
-    }, [medData]);
+        ProjectName: 'Hosting Press HTML',
+        Status: 'Active',
+        StatusBg: '#8BE78B',
+        Weeks: '40',
+        Budget: '$2.4k',
+        Location: 'India',
+    },
+    {
+        CustomerID: 1002,
 
-    return (
-        <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
-            <Header category="Page" title="Customers" />
-            <GridComponent
-                dataSource={customersData}
-                allowPaging
-                allowSorting
-                toolbar={['Delete']}
-                editSettings={{ allowDeleting: true, allowEditing: true }}
-                width="auto"
-            >
-                <ColumnsDirective>
-                    {medicineGrid.map((item, index) => (
-                        <ColumnDirective key={index}{...item} />
-                    ))}
-                </ColumnsDirective>
-                <Inject services={[Page, Toolbar, Selection, Edit, Sort, Filter]} />
-            </GridComponent>
-        </div >
-    )
-}
+        CustomerName: 'Sunil Joshi',
+        CustomerEmail: 'sunil@gmail.com',
+        ProjectName: 'Elite Admin',
+        Status: 'Active',
 
-export default Medicines
+        StatusBg: '#8BE78B',
+        Weeks: '11',
+        Budget: '$3.9k',
+        Location: 'India',
+    },
+    {
+        CustomerID: 1003,
 
+        CustomerName: 'Andrew McDownland',
+        CustomerEmail: 'andrew@gmail.com',
+        ProjectName: 'Real Homes WP Theme',
+        Status: 'Pending',
 
+        StatusBg: '#FEC90F',
+        Weeks: '19',
+        Budget: '$24.5k',
+        Location: 'USA',
+    },
+];
